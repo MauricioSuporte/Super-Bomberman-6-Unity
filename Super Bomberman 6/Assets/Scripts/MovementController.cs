e5464b4865cfc09794a4662c26c73c809e4c405d
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-11-18 22:27:53 -0300

    Implements sprit move animation

--- Super Bomberman 6/Assets/Scripts/MovementController.cs
+++ Super Bomberman 6/Assets/Scripts/MovementController.cs
@@ -11,38 +11,51 @@ public class MovementController : MonoBehaviour
     public KeyCode inputLeft = KeyCode.A;
     public KeyCode inputRight = KeyCode.D;
 
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
     }
 
     private void Update()
     {
         if (Input.GetKey(inputUp)) {
-            SetDirection(Vector2.up);
+            SetDirection(Vector2.up, spriteRendererUp);
         } else if (Input.GetKey(inputDown)) {
-            SetDirection(Vector2.down);
+            SetDirection(Vector2.down, spriteRendererDown);
         } else if (Input.GetKey(inputLeft)) {
-            SetDirection(Vector2.left);
+            SetDirection(Vector2.left, spriteRendererLeft);
         } else if (Input.GetKey(inputRight)) {
-            SetDirection(Vector2.right);
+            SetDirection(Vector2.right, spriteRendererRight);
         } else {
-            SetDirection(Vector2.zero);
+            SetDirection(Vector2.zero, activeSpriteRenderer);
         }
     }
 
     private void FixedUpdate()
     {
         Vector2 position = rigidbody.position;
-        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+        Vector2 translation = speed * Time.fixedDeltaTime * direction;
 
         rigidbody.MovePosition(position + translation);
     }
 
-    private void SetDirection(Vector2 newDirection)
+    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
     {
         direction = newDirection;
 
-        //...
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
     }
 }
