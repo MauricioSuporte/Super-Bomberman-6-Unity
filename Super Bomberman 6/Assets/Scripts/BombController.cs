c074eb475af5520bf2116c9663425fe8ab9455d4
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-11-20 16:00:31 -0300

    Add brick destructible logic

--- Super Bomberman 6/Assets/Scripts/BombController.cs
+++ Super Bomberman 6/Assets/Scripts/BombController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BombController : MonoBehaviour
 {
@@ -15,6 +16,10 @@ public class BombController : MonoBehaviour
     public LayerMask explosionLayerMask;
     public float explosionDuration = 1f;
     public int explosionRadius = 2;
+
+    [Header("Destructible")]
+    public Tilemap destructibleTiles;
+    public Destructible destructiblePrefab;
 
     private void OnEnable()
     {
@@ -76,6 +81,7 @@ public class BombController : MonoBehaviour
 
         if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
         {
+            ClearDestructible(position);
             return;
         }
 
@@ -85,5 +91,18 @@ public class BombController : MonoBehaviour
         explosion.DestroyAfter(explosionDuration);
 
         Explode(position, direction, length - 1);
+    }
+
+    private void ClearDestructible(Vector2 position)
+    {
+        Vector3Int cell = destructibleTiles.WorldToCell(position);
+
+        TileBase tile = destructibleTiles.GetTile(cell);
+
+        if ( tile != null)
+        {
+            Instantiate(destructiblePrefab, position, Quaternion.identity);
+            destructibleTiles.SetTile(cell, null);
+        }
     }
 }
