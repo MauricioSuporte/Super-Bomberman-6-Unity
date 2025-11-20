b4e3159d35b79003f3f7fd5fc1b5b511b758b6be
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-11-20 17:52:20 -0300

    Implements ItemPickup

--- Super Bomberman 6/Assets/Scripts/Destructible.cs
+++ Super Bomberman 6/Assets/Scripts/Destructible.cs
@@ -4,8 +4,21 @@ public class Destructible : MonoBehaviour
 {
     public float destructionTime = 1f;
 
+    [Range(0f, 1f)]
+    public float itemSpawnChance = 0.2f;
+    public GameObject[] spwnableItems;
+
     private void Start()
     {
         Destroy(gameObject, destructionTime);
+    }
+
+    private void OnDestroy()
+    {
+        if (spwnableItems.Length > 0 && Random.value < itemSpawnChance)
+        {
+            int randomIndex = Random.Range(0, spwnableItems.Length);
+            Instantiate(spwnableItems[randomIndex], transform.position, Quaternion.identity);
+        }
     }
 }
