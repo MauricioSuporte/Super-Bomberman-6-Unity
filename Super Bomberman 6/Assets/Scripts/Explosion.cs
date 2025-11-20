dea53b75327b382e0505bb84cf9af982ef457dcd
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-11-20 15:07:45 -0300

    Add explosion logic

---
+++ Super Bomberman 6/Assets/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public AnimatedSpriteRenderer start;
+    public AnimatedSpriteRenderer middle;
+    public AnimatedSpriteRenderer end;
+
+    public void SetActiveRenderer(AnimatedSpriteRenderer renderer)
+    {
+        start.enabled = renderer == start;
+        middle.enabled = renderer == middle;
+        end.enabled = renderer == end;
+    }
+
+    public void SetDirection(Vector2 diretion)
+    {
+        float angle = Mathf.Atan2(diretion.y, diretion.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+    }
+
+    public void DestroyAfter(float seconds)
+    {
+        Destroy(gameObject, seconds);
+    }
+}
