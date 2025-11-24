ab872f3d636adb3e438e1621551d688114057386
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-11-24 17:37:50 -0300

    Add animation when explode item

--- Super Bomberman 6/Assets/Scripts/ItemPickup.cs
+++ Super Bomberman 6/Assets/Scripts/ItemPickup.cs
@@ -11,6 +11,11 @@ public class ItemPickup : MonoBehaviour
 
     public ItemType type;
 
+    public AnimatedSpriteRenderer idleRenderer;
+    public AnimatedSpriteRenderer destroyRenderer;
+
+    private bool isBeingDestroyed = false;
+
     private void OnItemPickup(GameObject player)
     {
         switch (type)
@@ -37,7 +42,23 @@ public class ItemPickup : MonoBehaviour
         }
         else if (other.CompareTag("Explosion"))
         {
-            Destroy(gameObject);
+            DestroyWithAnimation();
         }
+    }
+
+    private void DestroyWithAnimation()
+    {
+        if (isBeingDestroyed) return;
+        isBeingDestroyed = true;
+
+        // desativa sprite normal do item
+        idleRenderer.enabled = false;
+
+        // ativa animação de destruição
+        destroyRenderer.enabled = true;
+        destroyRenderer.idle = false;
+
+        // destrói depois de 0.5s (ou tempo que quiser)
+        Destroy(gameObject, 0.5f);
     }
 }
