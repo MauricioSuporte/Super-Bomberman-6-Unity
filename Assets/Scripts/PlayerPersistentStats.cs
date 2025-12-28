3368c733f4ecd34a3402b628b20e3fb114fc9e8e
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-12-28 20:54:27 -0300

    Add Pink Bomber

--- Assets/Scripts/PlayerPersistentStats.cs
+++ Assets/Scripts/PlayerPersistentStats.cs
@@ -31,7 +31,7 @@ public static class PlayerPersistentStats
     public static bool HasFullFire = false;
 
     public static MountedLouieType MountedLouie = MountedLouieType.None;
-    public static BomberSkin Skin = BomberSkin.Green;
+    public static BomberSkin Skin = BomberSkin.Pink;
 
     public static float InternalSpeedToTilesPerSecond(int internalSpeed)
     {
