23db9cd8df63ef255b4e2234e68c57eba7946c2a
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2026-02-01 00:08:27 -0300

    Fix player sprite renderer active when riding

--- Assets/Scripts/PlayerMovimentation/PlayerRidingController.cs
+++ Assets/Scripts/PlayerMovimentation/PlayerRidingController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MovementController))]
@@ -19,6 +20,12 @@ public sealed class PlayerRidingController : MonoBehaviour
     Coroutine routine;
     bool isPlaying;
 
+    SpriteRenderer[] cachedSpriteRenderers;
+    AnimatedSpriteRenderer[] cachedAnimatedRenderers;
+
+    readonly HashSet<AnimatedSpriteRenderer> allowedAnims = new();
+    readonly HashSet<SpriteRenderer> allowedSrs = new();
+
     public bool IsPlaying => isPlaying;
 
     void Awake()
@@ -26,10 +33,26 @@ public sealed class PlayerRidingController : MonoBehaviour
         movement = GetComponent<MovementController>();
         bomb = GetComponent<BombController>();
 
+        CacheRenderers();
+        RebuildAllowedSets();
+
         SetAnimEnabled(ridingUp, false);
         SetAnimEnabled(ridingDown, false);
         SetAnimEnabled(ridingLeft, false);
         SetAnimEnabled(ridingRight, false);
+    }
+
+    void OnValidate()
+    {
+        RebuildAllowedSets();
+    }
+
+    void LateUpdate()
+    {
+        if (!isPlaying)
+            return;
+
+        EnforceRidingOnly();
     }
 
     public bool TryPlayRiding(Vector2 facing, System.Action onComplete = null, System.Action onStart = null)
@@ -42,12 +65,14 @@ public sealed class PlayerRidingController : MonoBehaviour
 
         isPlaying = true;
 
-        movement.SetInputLocked(true, forceIdle: true);
+        CacheRenderers();
+        RebuildAllowedSets();
 
+        movement.SetInputLocked(true, forceIdle: true);
         movement.SetAllSpritesVisible(false);
 
-
         var r = PickRidingRenderer(facing);
+
         DisableAllRiding();
 
         if (r != null)
@@ -65,6 +90,8 @@ public sealed class PlayerRidingController : MonoBehaviour
 
     IEnumerator FinishRoutine(System.Action onComplete)
     {
+        yield return null;
+
         yield return new WaitForSeconds(ridingSeconds);
 
         DisableAllRiding();
@@ -72,13 +99,73 @@ public sealed class PlayerRidingController : MonoBehaviour
         onComplete?.Invoke();
 
         movement.EnableExclusiveFromState();
-
         movement.SetInputLocked(false, forceIdle: true);
 
         isPlaying = false;
         routine = null;
     }
 
+    void CacheRenderers()
+    {
+        cachedSpriteRenderers = movement != null
+            ? movement.GetComponentsInChildren<SpriteRenderer>(true)
+            : GetComponentsInChildren<SpriteRenderer>(true);
+
+        cachedAnimatedRenderers = movement != null
+            ? movement.GetComponentsInChildren<AnimatedSpriteRenderer>(true)
+            : GetComponentsInChildren<AnimatedSpriteRenderer>(true);
+    }
+
+    void RebuildAllowedSets()
+    {
+        allowedAnims.Clear();
+        allowedSrs.Clear();
+
+        AddAllowed(ridingUp);
+        AddAllowed(ridingDown);
+        AddAllowed(ridingLeft);
+        AddAllowed(ridingRight);
+    }
+
+    void AddAllowed(AnimatedSpriteRenderer r)
+    {
+        if (r == null)
+            return;
+
+        allowedAnims.Add(r);
+
+        var srs = r.GetComponentsInChildren<SpriteRenderer>(true);
+        for (int i = 0; i < srs.Length; i++)
+            if (srs[i] != null)
+                allowedSrs.Add(srs[i]);
+    }
+
+    void EnforceRidingOnly()
+    {
+        if (cachedAnimatedRenderers != null)
+        {
+            for (int i = 0; i < cachedAnimatedRenderers.Length; i++)
+            {
+                var a = cachedAnimatedRenderers[i];
+                if (a == null) continue;
+
+                if (!allowedAnims.Contains(a) && a.enabled)
+                    a.enabled = false;
+            }
+        }
+
+        if (cachedSpriteRenderers != null)
+        {
+            for (int i = 0; i < cachedSpriteRenderers.Length; i++)
+            {
+                var sr = cachedSpriteRenderers[i];
+                if (sr == null) continue;
+
+                if (!allowedSrs.Contains(sr) && sr.enabled)
+                    sr.enabled = false;
+            }
+        }
+    }
 
     AnimatedSpriteRenderer PickRidingRenderer(Vector2 facing)
     {
