3f9d7927aea1bfb20fa1efd8cebfe2574fba053c
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2026-03-17 08:13:32 -0300

    Adjust buton icons in mobile

--- Assets/Scripts/Mobile/MobileButton.cs
+++ Assets/Scripts/Mobile/MobileButton.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MobileButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
 {
@@ -8,8 +9,33 @@ public class MobileButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     [SerializeField] private Vector3 releasedScale = Vector3.one;
     [SerializeField] private Vector3 pressedScale = new Vector3(0.9f, 0.9f, 1f);
 
+    [Header("Visual Fill")]
+    [SerializeField] private bool stretchVisualToHitbox = true;
+    [SerializeField] private bool disableVisualPreserveAspect = true;
+    [SerializeField] private bool disableVisualRaycastTarget = true;
+
+    [Header("Debug Hitbox")]
+    [SerializeField] private bool showDebugHitbox;
+    [SerializeField] private Color debugHitboxColor = new Color(1f, 0f, 0f, 0.20f);
+
+    private Image _hitboxImage;
+    private Color _originalColor;
+
     void Awake()
     {
+        _hitboxImage = GetComponent<Image>();
+
+        if (_hitboxImage != null)
+        {
+            _originalColor = _hitboxImage.color;
+
+            if (showDebugHitbox)
+                _hitboxImage.color = debugHitboxColor;
+            else
+                _hitboxImage.color = _originalColor;
+        }
+
+        ConfigureVisualToMatchHitbox();
         ApplyReleasedVisual();
     }
 
@@ -47,5 +73,47 @@ public class MobileButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     {
         if (visualTarget != null)
             visualTarget.localScale = releasedScale;
+    }
+
+    void ConfigureVisualToMatchHitbox()
+    {
+        if (visualTarget == null)
+            return;
+
+        if (stretchVisualToHitbox)
+        {
+            visualTarget.anchorMin = Vector2.zero;
+            visualTarget.anchorMax = Vector2.one;
+            visualTarget.pivot = new Vector2(0.5f, 0.5f);
+            visualTarget.anchoredPosition = Vector2.zero;
+            visualTarget.offsetMin = Vector2.zero;
+            visualTarget.offsetMax = Vector2.zero;
+            visualTarget.localScale = releasedScale;
+            visualTarget.localRotation = Quaternion.identity;
+        }
+
+        var visualImage = visualTarget.GetComponent<Image>();
+        if (visualImage != null)
+        {
+            if (disableVisualPreserveAspect)
+                visualImage.preserveAspect = false;
+
+            if (disableVisualRaycastTarget)
+                visualImage.raycastTarget = false;
+        }
+    }
+
+    void OnValidate()
+    {
+        var image = GetComponent<Image>();
+        if (image != null)
+        {
+            if (showDebugHitbox)
+                image.color = debugHitboxColor;
+            else if (Application.isPlaying)
+                image.color = _originalColor;
+        }
+
+        ConfigureVisualToMatchHitbox();
     }
 }
