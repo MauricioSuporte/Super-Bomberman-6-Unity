b833a5effc51c5413a7a3784392bbbc9b6396b8e
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2026-03-22 00:02:31 -0300

    Reset player stats when select a stage in world map

--- Assets/Scripts/WorldMap/WorldMapController.cs
+++ Assets/Scripts/WorldMap/WorldMapController.cs
@@ -394,6 +394,7 @@ public class WorldMapController : MonoBehaviour
         else
             yield return new WaitForSecondsRealtime(selectedTransitionDuration);
 
+        PlayerPersistentStats.ResetGameplayPersistenceToBaseValues();
         StagePreIntroPlayersWalk.SkipOnNextLoad();
         SceneManager.LoadScene(sceneName);
     }
