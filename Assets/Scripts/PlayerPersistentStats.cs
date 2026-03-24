a04875646b7407de085c86833d63ba1630f3c8a5
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2026-03-23 22:38:01 -0300

    Adjust player stats

--- Assets/Scripts/PlayerPersistentStats.cs
+++ Assets/Scripts/PlayerPersistentStats.cs
@@ -20,15 +20,15 @@ public static class PlayerPersistentStats
     public sealed class PlayerState
     {
         public int Life = 1;
-        public int BombAmount = 9;
-        public int ExplosionRadius = 10;
-        public int SpeedInternal = MaxSpeedInternal;
+        public int BombAmount = 1;
+        public int ExplosionRadius = 2;
+        public int SpeedInternal = MinSpeedInternal;
         public bool CanKickBombs = false;
         public bool CanPunchBombs = false;
         public bool HasPowerGlove = false;
         public bool CanPassBombs = false;
-        public bool CanPassDestructibles = true;
-        public bool HasPierceBombs = true;
+        public bool CanPassDestructibles = false;
+        public bool HasPierceBombs = false;
         public bool HasControlBombs = false;
         public bool HasPowerBomb = false;
         public bool HasRubberBombs = false;
