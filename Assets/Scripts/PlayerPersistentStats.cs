818d260e303141d6e60407f7660e8047ae2a368d
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2026-03-30 08:36:22 -0300

    Adjsut paladin and blocker die vsf

--- Assets/Scripts/PlayerPersistentStats.cs
+++ Assets/Scripts/PlayerPersistentStats.cs
@@ -23,18 +23,18 @@ public static class PlayerPersistentStats
         public int BombAmount = 9;
         public int ExplosionRadius = 10;
         public int SpeedInternal = MaxSpeedInternal;
-        public bool CanKickBombs = false;
-        public bool CanPunchBombs = true;
-        public bool HasPowerGlove = true;
+        public bool CanKickBombs = true;
+        public bool CanPunchBombs = false;
+        public bool HasPowerGlove = false;
         public bool CanPassBombs = false;
-        public bool CanPassDestructibles = true;
-        public bool HasPierceBombs = false;
-        public bool HasControlBombs = true;
+        public bool CanPassDestructibles = false;
+        public bool HasPierceBombs = true;
+        public bool HasControlBombs = false;
         public bool HasPowerBomb = false;
         public bool HasRubberBombs = false;
         public bool HasFullFire = false;
 
-        public MountedType MountedLouie = MountedType.None;
+        public MountedType MountedLouie = MountedType.Yellow;
         public BomberSkin Skin = BomberSkin.White;
 
         public readonly List<ItemType> QueuedEggs = new(8);
