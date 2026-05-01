5077773d63446292c0ffa938148a6fb4bd4b56a8
MauricioSuporte <mauriciobernardodesouza@gmail.com>
2026-05-01 15:14:25 -0400

    Add stun animation to player

--- Assets/Scripts/PlayerPersistentStats.cs
+++ Assets/Scripts/PlayerPersistentStats.cs
@@ -29,7 +29,7 @@ public static class PlayerPersistentStats
         public bool CanPassBombs = false;
         public bool CanPassDestructibles = true;
         public bool HasPierceBombs = true;
-        public bool HasControlBombs = true;
+        public bool HasControlBombs = false;
         public bool HasPowerBomb = false;
         public bool HasRubberBombs = false;
         public bool HasMagnetBomb = false;
