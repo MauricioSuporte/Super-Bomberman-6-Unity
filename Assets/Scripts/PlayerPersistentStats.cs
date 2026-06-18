c8c862b93a69ccff9bb738c3af176bc9a0efbcbe
MauricioSuporte <mauriciobernardodesouza@gmail.com>
2026-06-18 16:25:27 -0400

    Change same power up icons in normal game

--- Assets/Scripts/PlayerPersistentStats.cs
+++ Assets/Scripts/PlayerPersistentStats.cs
@@ -20,21 +20,21 @@ public static class PlayerPersistentStats
 
     public sealed class PlayerState
     {
-        public int Life = 1;
+        public int Life = 3;
         public int BombAmount = 5;
         public int ExplosionRadius = 9;
         public int SpeedInternal = BaseSpeedNormal + (SpeedStep * 5);
-        public bool CanKickBombs = true;
+        public bool CanKickBombs = false;
         public bool CanPunchBombs = true;
         public bool HasPowerGlove = true;
-        public bool CanPassBombs = false;
+        public bool CanPassBombs = true;
         public bool CanPassDestructibles = true;
         public bool HasPierceBombs = false;
-        public bool HasControlBombs = true;
+        public bool HasControlBombs = false;
         public bool HasPowerBomb = false;
         public bool HasRubberBombs = false;
-        public bool HasMagnetBomb = false;
-        public bool HasFullFire = false;
+        public bool HasMagnetBomb = true;
+        public bool HasFullFire = true;
 
         public MountedType MountedLouie = MountedType.None;
         public BomberSkin Skin = BomberSkin.White;
